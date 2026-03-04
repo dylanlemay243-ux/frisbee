1ad69fab47bda4a13f088c0cbcb9c740f35cc880
dylanlemay243-ux <dylanlemay243@gmail.com>
2026-03-04 14:42:29 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,13 @@
-const CACHE_NAME = "smart-frisbee-v1";
-
-const urlsToCache = [
-  "/frisbee/",
-  "/frisbee/index.html",
-  "/frisbee/manifest.json",
-  "/frisbee/icon-192.png",
-  "/frisbee/icon-512.png"
-];
+const CACHE_NAME = "smart-frisbee-v2";
 
 self.addEventListener("install", event => {
-
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
 });
 
 self.addEventListener("activate", event => {
-
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-
-  return self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-
+  event.respondWith(fetch(event.request));
 });
